2c03980308c3eae5ab956980b1f2e0563d3aa7fb
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2020-02-23 18:48:38 +0100

    Add menu options and scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,61 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Game game = new Game(1, "Easy");
+        play();
 
-        game.getNumberPlayers();
+//        game.getNumberPlayers();
+//
+//        Player annie = new Player("Annie", 0);
+//
+//        game.addPlayer(annie);
+//        game.getPlayers();
+//        game.addPlayer(annie);
+//
+//        game.splitWord("hello");
+//
+//        game.checkWord("hello", "hello", annie);
+    }
 
-        Player annie = new Player("Annie", 0);
+    private static void play(){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
 
-        game.addPlayer(annie);
-        game.getPlayers();
-        game.addPlayer(annie);
+        printMenu();
 
-        game.splitWord("hello");
+        while(!quit){
+            int action = scanner.nextInt();
+            scanner.nextLine();
 
-        game.checkWord("hello", "hello", annie);
+            switch(action){
+                case 0:
+                    System.out.println("Welcome to the word guessing game!");
+                    break;
+                case 1:
+                    System.out.println("Previous scores");
+                    break;
+                case 2:
+                    printMenu();
+                    break;
+                case 3:
+                    System.out.println("Quitting");
+                    quit = true;
+                    break;
+            }
+        }
+
+    }
+
+    private static void printMenu(){
+        System.out.println("Hello! Do you want to play the game?");
+        System.out.println("0 - to start new game\n" +
+                "1 - to see previous scores\n" +
+                "2 - print menu\n" +
+                "3 - to quit\n");
     }
 }
